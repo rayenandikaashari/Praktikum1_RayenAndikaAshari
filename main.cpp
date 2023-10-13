b4d29b941c7fe4e3dabbd27a5fca69a46338e55b
RayenAndikaAshari <2310631170043@student.unsika.ac.id>
2023-10-13 10:29:54 +0700

    menambahkan file .cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string namasaya;
+    namasaya = "Rayen Andika Ashari";
+    string namakelas;
+    namakelas = "1A - Informatika";
+    string npm;
+    npm = "2310631170043";
+    string citacita;
+    citacita = "ingin bisa menguasai semua bidang pemograman";
+    string motivasisaya;
+    motivasisaya = "karena saya dari dulu sangat mengagumi orang yang jago di bidang it";
+    cout << "-------------------------------------------------------------------------------\n";
+    cout << "Nama: " << namasaya << endl << endl;
+    cout << "Kelas: " << namakelas << endl << endl;
+    cout << "Npm: " << npm << endl << endl;
+    cout << "Cita-Cita: " << citacita << endl << endl;
+    cout << "Motivasi: " << motivasisaya << endl << endl;
+    cout << "-------------------------------------------------------------------------------\n";
+
+    return 0;
+}
